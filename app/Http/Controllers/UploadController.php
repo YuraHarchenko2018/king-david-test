d734949bf3219fdbc21a561fcdc5af301a2442f8
Yurii Kharchenko <ykharchenko@supportyourapp.com>
2020-07-20 21:36:24 +0300

    Returned lost file

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Jobs\TaskImageAfterLoadJob;
+
+class UploadController extends Controller
+{
+    public function store(Request $request)
+    {
+        $uid = $request->uid;
+        $image_name = 'image.png';
+
+        if($request->hasFile('image')) {
+            $request->file('image');
+            $request->image->storeAs('image', $uid . '/' . $image_name);
+
+            TaskImageAfterLoadJob::dispatch($image_name, $uid);
+        }
+    }
+}
